56087a77d7277ade8743915302cf095c60dd67e7
minkooksun <minkshep@gmail.com>
2024-05-14 07:25:35 +0900

    새로운 UI 적용

--- reflex_home/reflex_home/reflex_home.py
+++ reflex_home/reflex_home/reflex_home.py
@@ -1,57 +1,27 @@
 # reflex_home.py
-import reflex as rx
-
-from reflex_home import style
-from reflex_home.state import TutorialState
-
-
-def qa(question: str, answer: str) -> rx.Component:
-    return rx.box(
-        rx.box(
-            rx.text(question, text_align="right"),
-            style=style.question_style,
-        ),
-        rx.box(
-            rx.text(answer, text_align="left"),
-            style=style.answer_style,
-        ),
-    )
-
-
-def chat() -> rx.Component:
-    return rx.box(
-        rx.foreach(
-            TutorialState.chat_history,
-            lambda messages: qa(messages[0], messages[1]),
-        )
-    )
-
-
-def action_bar() -> rx.Component:
-    return rx.hstack(
-        rx.input(
-            placeholder="Ask a question",
-            value=TutorialState.question,
-            on_change=TutorialState.set_question,
-            style=style.input_style,
-        ),
-        rx.button(
-            "Ask",
-            on_click=TutorialState.answer,
-            style=style.button_style,
-        ),
-    )
+import reflex as rx 
+from reflex_home.components import chat, navbar
 
 
 def index() -> rx.Component:
-    return rx.center(
-        rx.vstack(
-            chat(),
-            action_bar(),
-            align="center",
-        )
+    """The main app."""
+    return rx.chakra.vstack(
+        navbar(),
+        chat.chat(),
+        chat.action_bar(),
+        background_color=rx.color("mauve", 1),
+        color=rx.color("mauve", 12),
+        min_height="100vh",
+        align_items="stretch",
+        spacing="0",
     )
 
 
-app = rx.App()
+# Add state and page to the app.
+app = rx.App(
+    theme=rx.theme(
+        appearance="dark",
+        accent_color="violet",
+    ),
+)
 app.add_page(index)
